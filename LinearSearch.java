ad8f5ffdc2ca619a02b109b5c6bd77f1f4a8348e
DishaSharma03 <144007432+DishaSharma03@users.noreply.github.com>
2023-09-04 14:53:24 +0530

    Update LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,1 +1,25 @@
-
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int bool=true;
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int t=sc.nextInt();
+        for(int i=0;i<n;i++){
+            if(arr[i]==t){
+                bool=true;
+                break;
+            }
+        }
+        if(bool==true){
+            System.out.println("Element found");
+        }
+        else{
+            System.out.println("Element not found");
+        }
+    }
+}
