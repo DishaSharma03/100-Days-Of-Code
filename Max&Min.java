146b208a06fe919b63669b920b828b01d0c197e9
DishaSharma03 <144007432+DishaSharma03@users.noreply.github.com>
2023-09-05 19:42:59 +0530

    Max&Min.java

--- Max&Min.java
+++ Max&Min.java
@@ -1,1 +1,14 @@
-
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println("Minimum element "+arr[0]);
+        System.out.println("Maximum element "+arr[n-1]);
+    }
+}
